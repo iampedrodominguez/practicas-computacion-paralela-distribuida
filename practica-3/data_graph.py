af4f38d7a84535d63712bbaaa4081e8798b2e1b4
PedroDominguezUTEC <pedro.dominguez@utec.edu.pe>
2022-09-23 23:28:02 -0500

    - graphs done

--- practica-3/data_graph.py
+++ practica-3/data_graph.py
@@ -3,9 +3,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 from os import path
 
-P = [i for i in range(2,17)]
+P = [i for i in range(1,17)]
 
-data = pd.read_csv('p1.csv', sep=',').to_numpy()
+data = pd.read_csv('p2-b.csv', sep=',').to_numpy()
 data = data[:,1:]
 
 prom = []
@@ -13,10 +13,10 @@ for list_ in data:
     prom.append(np.mean(list_))
 
 # Plot data
-plt.title('Processors vs Time para N = 4096')
-plt.plot(P, prom, label='Comunication time')
+plt.title('Processors vs Time para N = 1024')
+plt.plot(P, prom, label='Latency time')
 plt.legend()
 plt.xlabel('Processors')
 plt.ylabel('Time (ms)')
-plt.savefig('graphPvsT.png')
+plt.savefig('graphPvsT-2b.png')
 

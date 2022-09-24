65a7dee7dfd12d2ea77dd0c4d40bd8d8aa00a479
jneirar <jorge.neira@utec.edu.pe>
2022-09-23 21:56:19 -0500

    graph p1

---
+++ practica-3/data_graph.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from os import path
+
+P = [i for i in range(2,17)]
+
+data = pd.read_csv('p1.csv', sep=',').to_numpy()
+data = data[:,1:]
+
+prom = []
+for list_ in data:
+    prom.append(np.mean(list_))
+
+# Plot data
+plt.title('Processors vs Time para N = 4096')
+plt.plot(P, prom, label='Comunication time')
+plt.legend()
+plt.xlabel('Processors')
+plt.ylabel('Time (ms)')
+plt.savefig('graphPvsT.png')
+

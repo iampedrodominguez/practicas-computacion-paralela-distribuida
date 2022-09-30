09fd9abc73da42a7011f6d401cb09bc15eceaf1b
PedroDominguezUTEC <pedro.dominguez@utec.edu.pe>
2022-09-30 17:20:45 -0500

    - qs not working well

--- practica-04/qsort.c
+++ practica-04/qsort.c
@@ -10,7 +10,7 @@ void swap(int *x, int *y){
 
 int separate(int *x, int low, int high){
   int i, pivot, last;
-  pivot = x[low]; 
+  pivot = x[low];
   swap(x+low, x+high);
   last = low;
   for(i = low; i<high; i++){
@@ -24,14 +24,32 @@ int separate(int *x, int low, int high){
 }
 
 void qs(int *z, int zstart, int zend, int firstcall){
-  {int pivot;
-    if(firstcall == 1) {
-      qs(z,0,zend,0);
-    } else {
-      if(zstart<zend) {
-	pivot = separate(z, zstart,zend);
-	qs(z, zstart, pivot-1,0);
-	qs(z,pivot+1,zend,0);
+  #pragma omp parallel
+  {
+    int pivot;
+    
+    #pragma omp single
+    if (firstcall == 1) 
+    {
+      qs(z, 0, zend, 0);
+    
+    } 
+    else
+    {
+      if (zstart < zend) 
+      {
+      	pivot = separate(z, zstart, zend);
+	      #pragma omp task
+        {
+          printf("Thread working in task: %i\n", omp_get_thread_num());
+          qs(z, zstart, pivot - 1, 0);
+        }
+
+        #pragma omp task 
+        {
+          printf("Thread working in task: %i\n", omp_get_thread_num());
+	        qs(z, pivot + 1, zend, 0);
+        }
       }
     }
   }
@@ -41,9 +59,14 @@ int main(int argc, char **argv){
   int i,n,*w;
   n = atoi(argv[1]);
   w = malloc(n*sizeof(int));
+
+  omp_set_num_threads(12);
+
   for(i = 0; i<n; i++) w[i] = rand()%100;
+  for(i=0;i<n;i++) printf("%d\n",w[i]);
+  printf("\n");
   qs(w,0,n-1,1);
-  if(n<25)
+  if(n<250)
     for(i=0;i<n;i++) printf("%d\n",w[i]);
   return 0;
 } 

886c378830152434cc195bbe2cdeb934fa19f11f
BTbackM <felix.blanco@utec.edu.pe>
2022-10-06 16:00:36 -0500

    fix: P3 dependecies and tasks

--- practica-04/P3/p3.cpp
+++ practica-04/P3/p3.cpp
@@ -2,6 +2,7 @@
 #include "functions.cpp"
 #include <fstream>
 #include <iomanip>
+#include <algorithm>
 
 #define x 0
 int P = 4;
@@ -36,8 +37,8 @@ double OpArr(double *A, int *B, double *C, int n)
 
   prefix_sum<double>(A, n); // obtiene el array de suma de prefijos de A
 
-#pragma omp parallel
-#pragma omp single
+  #pragma omp parallel
+  #pragma omp single
   qs(B, 0, n - 1, 1); // ordena el array B
 
   scan_left<double>(C, x, n); // acumula los valores de elementos de C mas una constante x = 100
@@ -46,45 +47,40 @@ double OpArr(double *A, int *B, double *C, int n)
   prnt(A, B, C, n);
 #endif
 
-#pragma omp parallel
-#pragma omp single
-  for (i = 0; i < n; i++)
-  {
+  #pragma omp parallel default(none) shared(A, B, n, s1)
+  #pragma omp single private(i)
+  for(i = 0; i < n; i++) {
     // primer bucle for
-    s1 = 0;
+    s1 = 0.0;
 
-#pragma omp task depend(in                 \
-                        : A, B) depend(out \
-                                       : s1)
-    for (j = 0; j < n; j++)
+    #pragma omp task private(j) depend(inout: s1) depend(in: A, B)
+    for(j = 0; j < n; j++)
+      #pragma omp atomic
       s1 += A[j] * B[j];
 
-#pragma omp task depend(in               \
-                        : s1) depend(out \
-                                     : A)
-    for (j = 0; j < n; j++)
+    #pragma omp task private(j) depend(in: s1) depend(inout: A)
+    for(j = 0; j < n; j++)
+      #pragma omp atomic
       A[j] *= s1;
   }
 
-#pragma omp parallel
-#pragma omp single
-  for (i = 0; i < n; i++)
-  {
+  #pragma omp parallel default(none) shared(B, C, n, s2)
+  #pragma omp single private(i)
+  for(i = 0; i < n; i++) {
     // segundo bucle for
-    s2 = 0;
+    s2 = 0.0;
 
-#pragma omp task depend(in                 \
-                        : B, C) depend(out \
-                                       : s2)
-    for (j = 0; j < n; j++)
+    #pragma omp task private(j) depend(inout: s2) depend(in: B, C)
+    for(int j = 0; j < n; j++)
+      #pragma omp atomic
       s2 += B[j] * C[j];
 
-#pragma omp task depend(in               \
-                        : s2) depend(out \
-                                     : C)
-    for (j = 0; j < n; j++)
+    #pragma omp task private(j) depend(in: s2) depend(inout: C)
+    for(int j = 0; j < n; j++)
+      #pragma omp atomic
       C[j] *= s2;
   }
+
   // calculo final
   a = s1 / s2;
 #ifdef _DEBUG
@@ -94,13 +90,6 @@ double OpArr(double *A, int *B, double *C, int n)
 #endif
   res = 0;
 
-#pragma omp task depend(in \
-                        : a, C)
-  for (i = 0; i < n; i++)
-    res += a * C[i];
-  a = s1 / s2;
-
-  res = 0;
   for (i = 0; i < n; i++)
     res += a * C[i];
 
@@ -168,7 +157,7 @@ int main(int argc, char *argv[])
 
 // Define n
 #ifdef _DEBUG
-  n = 2;
+  n = 4;
 #else
   n = (int)atoi(argv[1]);
 #endif
@@ -191,12 +180,23 @@ int main(int argc, char *argv[])
   }
 
 #ifdef _DEBUG
-  prnt(A, B, C, n);
-  res = OpArrSeq(A, B, C, n);
+  double * _A, *_C;
+  int * _B;
+  _A = new double[n];
+  _B = new int[n];
+  _C = new double[n];
+  std::copy(A, A + n, _A);
+  std::copy(B, B + n, _B);
+  std::copy(C, C + n, _C);
+  res = OpArrSeq(_A, _B, _C, n);
   std::cout << "\tResultadoSeq: " << res << std::endl;
 
-  // res = OpArr(A, B, C, n);
-  // std::cout << "\tResultado: " << res << std::endl;
+  res = OpArr(A, B, C, n);
+  std::cout << "\tResultado: " << res << std::endl;
+
+  delete[] _A;
+  delete[] _B;
+  delete[] _C;
 #else
   auto t0 = std::chrono::steady_clock::now();
   res = OpArr(A, B, C, n);

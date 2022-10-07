9cadb1bfd4364159a43a6fe07099d9fb043ed023
PedroDominguezUTEC <pedro.dominguez@utec.edu.pe>
2022-10-07 07:44:07 -0500

    - script to get times for p2

--- practica-04/P2/p2.cpp
+++ practica-04/P2/p2.cpp
@@ -1,6 +1,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <omp.h>
+#include <iostream>
+#include <fstream>
+#include <chrono>
+#include <unistd.h>
+#include <iomanip>
 
 void swap(int *x, int *y){
   int temp = *x;
@@ -40,13 +45,13 @@ void qs(int *z, int zstart, int zend, int firstcall){
         
 	      #pragma omp task
         {
-          printf("Thread working in task: %i\n", omp_get_thread_num());
+          //printf("Thread working in task: %i\n", omp_get_thread_num());
           qs(z, zstart, pivot - 1, 0);
         }
 
         #pragma omp task 
         {
-          printf("Thread working in task: %i\n", omp_get_thread_num());
+          //printf("Thread working in task: %i\n", omp_get_thread_num());
 	        qs(z, pivot + 1, zend, 0);
         }
       }
@@ -55,23 +60,34 @@ void qs(int *z, int zstart, int zend, int firstcall){
 }
 
 int main(int argc, char **argv){
-  int i,n,*w;
-  n = atoi(argv[1]);
-  w = malloc(n*sizeof(int));
 
-  omp_set_num_threads(12);
+  //Initialize variables
+  int i,n,*w, threads;
+  n = 1000000;
+  threads = atoi(argv[1]);
+  w = new int[n];
+
+  omp_set_num_threads(threads);
 
   for(i = 0; i<n; i++) w[i] = rand()%100;
-  for(i=0;i<n;i++) printf("%d\n",w[i]);
-  printf("\n");
   
-  #pragma omp parallel 
+  //Get parallel execution time
+  auto t0 = std::chrono::steady_clock::now();
+
+  #pragma omp parallel
   {
     #pragma omp single
     qs(w,0,n-1,1);
   }
 
-  if(n<250)
-    for(i=0;i<n;i++) printf("%d\n",w[i]);
+  auto t1 = std::chrono::steady_clock::now();
+  auto dtime = std::chrono::duration_cast<std::chrono::milliseconds>(t1 - t0).count();
+
+  // Save parallel time in .csv
+  std::ofstream myfile;
+  myfile.open("p2.csv", std::ios::app);
+  myfile << ", " << std::fixed << std::setprecision(4) << dtime;
+  myfile.close();
+  
   return 0;
 } 
---
+++ practica-04/P2/test2.bash
@@ -0,0 +1,10 @@
+g++ -fopenmp p2.cpp -o p2
+
+for p in `seq 1 2 4 8 16 32 64`
+do
+   echo "$a" >> p2.csv
+   for i in {1..10}
+   do
+      ./p2 $p
+   done
+done

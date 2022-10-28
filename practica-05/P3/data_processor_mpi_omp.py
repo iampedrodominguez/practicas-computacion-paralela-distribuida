8fd9563cee92e64de7784e06b817da40c39d9ae1
BT <felix.blanco@utec.edu.pe>
2022-10-28 00:15:46 -0500

    feat: P03 D graphs

--- practica-05/P3/data_processor_mpi_omp.py
+++ practica-05/P3/data_processor_mpi_omp.py
@@ -5,40 +5,43 @@ from os import path
 
 p_min = 1
 p_max = 5
+t_min = 1
+t_max = 5
 
 N_constant = 1024
 
 P = [2 ** i for i in range(p_min, p_max + 1)]
-
-t_comm = []
-t_comp = []
-t_total = []
+T = [2 ** i for i in range(t_min, t_max + 1)]
 
 data_dir = input("Dir: ")
 for p in P:
-    path_str = f'{data_dir}/data_{N_constant}_{p}.csv'
-    if not path.exists(path_str):
-        continue
-    # Read data from file
-    data = pd.read_csv(path_str, sep=',', header=None)
-    # Get average of each column
-    avg = data.mean(axis=0)
-    avg = np.array(avg)
+    t_comm = []
+    t_comp = []
+    t_total = []
+    for t in T:
+        path_str = f'{data_dir}/data_{N_constant}_{t}_{p}.csv'
+        if not path.exists(path_str):
+            continue
+        # Read data from file
+        data = pd.read_csv(path_str, sep=',', header=None)
+        # Get average of each column
+        avg = data.mean(axis=0)
+        avg = np.array(avg)
 
-    # Get averages
-    t_comm.append(avg[0])
-    t_comp.append(avg[1])
-    t_total.append(avg[2])
+        # Get averages
+        t_comm.append(avg[0])
+        t_comp.append(avg[1])
+        t_total.append(avg[2])
 
-# Plot data
-plt.title('Processors vs Time: N = 1024', fontweight = 'bold')
-plt.plot(P, t_comm, 'o--', label = 'Comunication time')
-plt.plot(P, t_comp, 'o--', label = 'Computation time')
-plt.plot(P, t_total, 'o--', label = 'Total time')
-plt.legend()
-plt.xlabel('Processors', fontweight = 'bold')
-plt.ylabel('Time (ms)', fontweight = 'bold')
-plt.savefig('mpi_graph_execution_vs_processors.png', dpi=1000)
+    # Plot data
+    plt.title(f'Threads vs Time with P = {p}:\n N = {N_constant}', fontweight = 'bold')
+    plt.plot(P, t_comm, 'o--', label = 'Comunication time')
+    plt.plot(P, t_comp, 'o--', label = 'Computation time')
+    plt.plot(P, t_total, 'o--', label = 'Total time')
+    plt.legend()
+    plt.xlabel('Threads', fontweight = 'bold')
+    plt.ylabel('Time (ms)', fontweight = 'bold')
+    plt.savefig(f'mpi_omp_graph_execution_vs_threads_{p}.png', dpi=1000)
 
-# Clean plot
-plt.clf()
+    # Clean plot
+    plt.clf()

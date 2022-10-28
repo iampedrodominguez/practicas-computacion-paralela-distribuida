696623b9c376e24d58326cf1b64ef3591812a22a
educscs <109014596+educscs@users.noreply.github.com>
2022-10-28 03:51:19 -0400

    bucketsort

--- practica-05/P2/bucketsort.cpp
+++ practica-05/P2/bucketsort.cpp
@@ -1,3 +1,6 @@
+//#include <regex>
+
+#include<bits/stdc++.h>
 #include <omp.h>
 #include <math.h>
 #include <time.h>
@@ -6,7 +9,7 @@
 #include <stdlib.h>
 #include <iostream>
 #include <algorithm>
-#include <chrono>
+
 #include <iomanip>
 using namespace std;
 void bucketSort(float *arr, int &n, int bucket_size)
@@ -34,63 +37,85 @@ void bucketSort(float *arr, int &n, int bucket_size)
     }
 }
 
-void bucketSortParallel(float *arr, int &n, int bucket_size)
+void bucketSortParallel(float *arr, int &n, int bucket_size, int number_of_threads)
 {
     // Crear buckets
     std::vector<float> bucket[bucket_size];
     int i, j, bucket_index, index = 0;
-    omp_set_num_threads(16);
-#pragma omp parallel
-    {
+    omp_set_num_threads(number_of_threads);
+
+    
 // asignar elementos a los buckets
-#pragma omp for private(i, bucket_index)
+#pragma omp parallel 
+     {   
+        #pragma omp for private(i,bucket_index) 
         for (i = 0; i < n; ++i)
         {
             bucket_index = bucket_size * arr[i] / 1000;
-#pragma omp critical
+            #pragma omp critical
             bucket[bucket_index].push_back(arr[i]);
         }
-#pragma omp barrier
-
+        
+        #pragma omp barrier
 // ordenar buckets
-#pragma omp for private(j)
+         #pragma omp for private(j)
         for (j = 0; j < bucket_size; ++j)
             sort(bucket[j].begin(), bucket[j].end());
 // Concatenar buckets en arr[]
-#pragma omp barrier
+     }
+
         for (i = 0; i < bucket_size; i++) {
     for (j = 0; j < bucket[i].size(); j++)
       arr[index++] = bucket[i][j];
   }
-    }
+    
 }
 
 int main(int argc, char *argv[])
 {
-    int i, n = pow(2, 4);
+    int i, n = pow(2, 19);
     float *randArray;
-    // omp_set_num_threads(8);
 
     srand((int)time(0));
 
     randArray = new float[n];
+    
+    int num_of_iterations = 10;
 
-    for (int i = 0; i < n; ++i)
+    for (int i = 0; i < n; ++i){
         randArray[i] = (float)rand() / (float)(RAND_MAX / 999.0);
+    }
 
-    for (i = 0; i < n; ++i)
-        printf("%1.2f, ", randArray[i]);
-    printf("\n");
-    auto randArray1 = randArray;
-    // bucketSort(randArray, n, 8);
-    auto t0 = chrono::steady_clock::now();
-    bucketSortParallel(randArray, n, 8);
-    auto t1 = std::chrono::steady_clock::now();
-    auto dtime = std::chrono::duration_cast<chrono::milliseconds>(t1 - t0).count();
-    cout << "Tiempo paralelo : " << dtime << " ms" << std::endl;
-    for (i = 0; i < n; ++i)
-        printf("%1.2f, ", randArray[i]);
+    int num_of_process = 12;
+    int* total_of_process;
+    total_of_process = new int[num_of_process];
+    
+    int m=1;
+    for(int i=0;i<num_of_process;i++){
+       total_of_process[i]=m;
+       m*=2;
+    }
+    
+    double * answer = new double[num_of_process]; 
+    for(int i=0;i<num_of_process;i++){
+        double sum=0;
+        for(int j=0;j<10;j++){
+           double t1, t2;
+           int s = total_of_process[i];
+           t1 = omp_get_wtime();
+           
+           bucketSortParallel(randArray, n, 8,s);
+           t2 = omp_get_wtime();
+           sum = sum + (t2-t1);
+        }
+        sum = sum/10.0;
+        answer[i] = sum;
 
+    }
+    for(i =0;i<num_of_process;i++){
+        printf("%f8 ", answer[i]);
+    }
     delete[] randArray;
+    delete[] total_of_process;
+    delete[] answer;
 }
-

af88a7d00c7d863e1a0dfa73bd87db0c613e9bf7
educscs <109014596+educscs@users.noreply.github.com>
2022-11-24 01:58:15 -0500

    add tsp parallel

---
+++ proyecto/tsp_parallel.cpp
@@ -0,0 +1,205 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <climits>
+#include <algorithm>
+#include <math.h>
+#include <fstream>
+#include <omp.h>
+using namespace std;
+
+#define INF INT_MAX
+
+
+typedef vector<vector<int>> Graph;
+class Node {
+public:
+    Graph graph;
+    vector<int> path;
+    int cost;
+    int curr;
+    int cost_parent;
+
+    Node() {
+        cost = 0;
+        curr = 0;
+        cost_parent = 0;
+    }
+};
+
+class TSP
+{
+private:
+    int n;
+    Graph graph;
+    Node* root;
+    vector<int> path;
+
+    void reduceGraph(Node* node);
+    void reduce(Node* node, int from, int to);
+
+public:
+    TSP(int n, Graph graph);
+    void printGraph();
+    void solve();
+    bool printSolution();
+    bool printSolutionToFile(string filename);
+};
+
+TSP::TSP(int n, Graph graph)
+{
+    this->n = n;
+    this->graph = graph;
+    
+    root = new Node;
+    root->graph = graph;
+    root->path = {0};
+    root->cost = 0;
+    root->curr = 0;
+    root->cost_parent = 0;
+}
+
+void TSP::reduceGraph(Node* node)
+{
+    int cost = 0;
+    //for each row
+    #pragma omp parallel for shared(node) reduction(+:cost) num_threads(8)
+    for(int i=0; i<n; i++)
+    {
+        int mn = *min_element(node->graph[i].begin(), node->graph[i].end());
+        if(mn != INF)
+        {   
+            cost += mn;
+            //#pragma omp parallel for shared(node,graph) num_threads(2)
+            for(int j=0; j<n; j++){
+                if(node->graph[i][j] != INF){
+                    node->graph[i][j] -= mn;
+                }
+            }
+        }
+    }
+    //for each column
+
+    #pragma omp parallel for shared(node) reduction(+:cost)  num_threads(8)
+    for(int i=0; i<n; i++)
+    {
+        int mn = INF;
+        for(int j=0; j<n; j++)
+            if(node->graph[j][i] < mn)
+                mn = node->graph[j][i];
+        if(mn != INF)
+        {    
+            cost += mn;
+            for(int j=0; j<n; j++)
+                if(node->graph[j][i] != INF)
+                    node->graph[j][i] -= mn;
+        }
+    }
+    node->cost += cost;
+}
+
+void TSP::reduce(Node* node, int from, int to) {
+    node->cost += node->graph[from][to] + node->cost_parent;
+    int i,j;
+    //#pragma omp parallel for shared(node,graph) num_threads(8)
+    for (i = 0; i < n; i++) 
+    node->graph[from][i] = INT_MAX;
+    //#pragma omp parallel for shared(node,graph) num_threads(8)
+    for (j = 0; j < n; j++) node->graph[j][to] = INT_MAX;
+    node->graph[to][0] = INT_MAX;
+    reduceGraph(node);
+}
+
+
+void TSP::solve()
+{
+    path = {0};
+    reduceGraph(root);
+
+    auto cmp = [](Node* a, Node* b) {
+        return a->cost > b->cost;
+    };
+    priority_queue<Node*, vector<Node*>, decltype(cmp)> pq(cmp);
+    pq.push(root);
+
+    while (!pq.empty()) {
+        Node* cur = pq.top();
+        pq.pop();
+        int from = cur->curr;
+        if (cur->path.size() == n) {
+            // Find a solution
+            cur->path.push_back(0);
+            path = cur->path;
+            break;
+        }
+        //#pragma omp parallel for 
+        for(int to = 0; to < n; to++){
+            if(cur->graph[from][to] == INF)
+                continue;
+            //if to is elegible
+            Node* child = new Node();
+            child->graph = cur->graph;
+            child->path = cur->path;
+            child->path.push_back(to);
+            child->curr = to;
+            child->cost_parent = cur->cost;
+
+            reduce(child, from, to);
+            pq.push(child);
+        }
+
+        delete cur;
+    }
+} 
+
+bool TSP::printSolution()
+{
+    if(path.size() == 0) {
+        printf("Make solve to generate a solution\n");
+        return 0;
+    }
+    int cost = 0;
+    int prev = -1;
+    printf("Solution: ");
+    for (int &i : path) {
+        printf("%d ", i);
+        if (prev != -1) cost += graph[prev][i];
+        prev = i;
+    }
+    printf("\nCost: %d\n", cost);
+    return 1;
+}
+
+bool TSP::printSolutionToFile(string filename)
+{
+    if(!printSolution()) return 0;
+    
+    int cost = 0;
+    int prev = -1;
+    for (int &i : path) {
+        if (prev != -1) cost += graph[prev][i];
+        prev = i;
+    }
+    
+    ofstream file;
+    file.open(filename, ios::out);
+    if(file.fail()) return 0;
+    file << cost << "\n";
+    for (int &i : path) {
+        file << i + 1 << "\n";
+    }
+    file.close();
+    return 1;
+}
+
+void TSP::printGraph()
+{
+    printf("Graph of size %d:\n", n);
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++)
+            printf("%d ", graph[i][j]);
+        printf("\n");
+    }
+    printf("\n");
+}

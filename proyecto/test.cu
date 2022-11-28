619a7558b0077a926eeec1af3c1a8901b785dc67
Pedro Francisco Dominguez Aspilcueta <pedro.dominguez@khipu.utec.edu.pe>
2022-11-27 20:48:03 -0500

    - test working

--- proyecto/test.cu
+++ proyecto/test.cu
@@ -4,11 +4,12 @@
 #define INF INT_MAX
 using namespace std;
 
+#define N 10
 typedef vector<vector<int>> Graph;
 
 __device__ int cost;
 
-__global__ void test(Graph graph, int n)
+__global__ void test(int graph[], int n)
 
 {
     //Get thread ID.x
@@ -20,8 +21,8 @@ __global__ void test(Graph graph, int n)
 
         //Find the smallest row value
         for (int j = 0; j < n; j++)
-            if (graph[idx][j] < mn)
-                mn = graph[idx][j];
+            if (graph[idx * n + j] < mn)
+                mn = graph[idx * n + j];
         
         //If small value is valid
         if (mn != INF && mn > 0)
@@ -30,8 +31,8 @@ __global__ void test(Graph graph, int n)
 
             //Reduce the cost for each row element
             for(int j=0; j<n; j++){
-                if(graph[idx][j] != INF){
-                    graph[idx][j] -= mn;
+                if(graph[idx * n + j] != INF){
+                    graph[idx * n + j] -= mn;
                 }
             }
         }
@@ -41,38 +42,47 @@ __global__ void test(Graph graph, int n)
 int main(){
 
     int* cost;
-    int n = 10;
+    const int n = 10;
 
-    //Allocatig GPU memory for each matrix
-    Graph CPUgraph = vector<vector<int>>(vector<int>(10),10);
-    Graph* GPUgraph = new Graph(10,10,0);
+    //Allocatig GPU memory for matrix
+   
+    int* CPUgraph = new int[n * n];
+    int* GPUgraph = new int[n * n];
+
+
+    for (int i = 0 ; i < n; i++) {
+        for (int j = 0; j < n; j++)
+            CPUgraph[i * n + j] = 10;
+    }
 
     size_t bytes_i = n * n * sizeof(int);
-    cudaMalloc((void**)GPUgraph, bytes_i);
 
-    //Allocating GPU memory for global variable cost
-    cudaMalloc(&cost, sizeof(int));
+    cudaMalloc((void**)&GPUgraph, bytes_i);
+
+    //Allocating GPU memory for global variable COST
+    cudaMalloc((void**)&cost, sizeof(int));
 
     //Pensar como pasarlo a GPU
-    cudaMemcpy(GPUgraph, &(CPUgraph), bytes_i, cudaMemcpyHostToDevice); 
+    cudaMemcpy(GPUgraph, CPUgraph, bytes_i, cudaMemcpyHostToDevice); 
 
     //Declare dimensions of GPU grid
     dim3 BLOCKS(n, n);
 
     //Call reduce functions in GPU
-    test<<<1, BLOCKS>>>(*GPUgraph, n);
+    test<<<1, BLOCKS>>>(GPUgraph, n);
 
+    //cout << GPUgraph[0];
     //Return matrix
-    if (cudaMemcpy(&(CPUgraph), GPUgraph, bytes_i, cudaMemcpyDeviceToHost) == cudaSuccess){
-        printf("Se pudo copiar a imagen final\n");
+    if (cudaMemcpy(CPUgraph, GPUgraph, bytes_i, cudaMemcpyDeviceToHost) == cudaSuccess){
+        cout << "Se pudo copiar a matriz final\n";
     } 
 
-    for (auto row : CPUgraph) {
-       for (auto col : row)
-              cout << col << " ";
-       cout << endl;
-    }
 
+    for (int i = 0 ; i < n; i++) {
+        for (int j = 0; j < n; j++)
+            cout << CPUgraph[i * n + j] << " ";
+        cout << endl;
+    }
     //Free memory from GPU
     cudaFree(GPUgraph);
     cudaFree(cost);
